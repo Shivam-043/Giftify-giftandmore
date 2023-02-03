88bfe06352a6df08755b409248857601ad67461c
Shivam Varshney <12111043@nitkkr.ac.in>
2023-02-03 23:35:28 +0530

    backend changes

--- frontend/costumer_detail/detail.js
+++ frontend/costumer_detail/detail.js
@@ -1,6 +1,6 @@
 // const Server = "http://15.207.71.90:3001";
 // const Server = "http://127.0.0.1:3001";
-const Server = "http://15.207.71.90:3001";
+// const Server = "http://15.207.71.90:3001";
 
 
 // formAction();
--- frontend/script.js
+++ frontend/script.js
@@ -18,7 +18,7 @@ var brand;
 
 // const Server = "http://15.207.71.90:3001";
 // const Server = "http://127.0.0.1:3001";
-const Server = "http://15.207.71.90:3001";
+// const Server = "http://15.207.71.90:3001";
 
 
 localStorage.removeItem("addons");
