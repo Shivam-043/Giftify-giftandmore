7f5d9fb747d52a53e0cd8b7675347bd559be86a6
Shivam Varshney <12111043@nitkkr.ac.in>
2023-02-03 23:43:31 +0530

    backend changes

--- frontend/apple.js
+++ frontend/apple.js
@@ -1,4 +1,4 @@
 // const Server = "http://15.207.71.90:3001";
-const Server = "http://127.0.0.1:3001";
-// const Server = "http://15.207.71.90:3001";
+// const Server = "http://127.0.0.1:3001";
+const Server = "http://15.207.71.90:3001";
 
